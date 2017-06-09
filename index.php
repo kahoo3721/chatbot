b626cbe026d572208913df4e140efdd02c549631
kaho_inoue-PC\kaho_inoue <kahoinoue123@gmail.com>
2017-06-09 23:53:08 +0900

    reply massage

--- index.php
+++ index.php
@@ -12,8 +12,23 @@ $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATUR
 
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
+// 配列に格納された各イベントをループで処理
 foreach ($events as $event) {
-   $bot->replyText($event->getReplyToken(), 'TextMessage';
+  //テキストを返信し次のイベント処理へ
+  replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
+}
+
+
+  // テキストを返信。引数はLINEBot、返信先、テキスト
+  function replyTextMessage($bot, $replyToken, $text) {
+    // 返信を行いレスポンスを取得
+    // TextMessageBuilderの引数はテキスト
+    $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+    // レスポンスが異常な場合
+    if (!$response->isSucceeded()) {
+      // エラー内容を出力
+      error_log('Failed! '. $response->getHTTPStatus . ' ' . $response->getRawBody());
+    }
   }
 
 
