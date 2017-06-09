b87e84c5423fe6006f58eb933ecf711d46df5818
kaho_inoue-PC\kaho_inoue <kahoinoue123@gmail.com>
2017-06-09 22:44:43 +0900

    テキスト

--- index.php
+++ index.php
@@ -1,22 +1,17 @@
 <?php
 
-require_once__DIR__ . '/vendor/autoload.php';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv
-                      ('CHANNEL_ACCESS_TOKEN'));
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv
-                      ('CHANNEL_SECRET')]);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\constant\HTTPHeader
-::LINE_SIGNATURE];
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
-$events = $bot->parseEventRequest(file_get_contents('php://input'),
-$signature);
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
 foreach ($events as $event) {
-  //テキストを送信
-  $bot->replyText($event->getReplyToken(), 'TextMessage');
-}
+   $bot->replyText($event->getReplyToken(), 'TextMessage';
+  }
+
 
 ?>
