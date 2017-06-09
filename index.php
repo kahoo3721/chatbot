768484362326e924bc3e5d80a66be8c8e03fd22a
kaho_inoue-PC\kaho_inoue <kahoinoue123@gmail.com>
2017-06-09 22:19:35 +0900

    テキスト

--- index.php
+++ index.php
@@ -2,7 +2,21 @@
 
 require_once__DIR__ . '/vendor/autoload.php';
 
-$inputString = file_get_contents('php://input');
-error_log($inputString);
+$httpClient = new \LINE\LINEBot\httpClient\CurlHTTPClient(getenv
+('CHANNEL_ACCESS_TOKEN'));
+
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv
+('CHANNEL_SECRET')]);
+
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\constant\HTTPHeader
+::LINE_SIGNATURE];
+
+$events = $bot->parseEventRequest(file_get_contents('php://input'),
+$signature);
+
+foreach ($events as $event) {
+  //テキストを送信
+  $bot->replyText($event->getReplyToken(), 'TextMessage');
+}
 
 ?>
